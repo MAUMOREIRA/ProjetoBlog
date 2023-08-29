861a61a4988f7f472e26850e66a9f82068b4a4a9
MAUMOREIRA <mauriciomoreirajr2006@gmail.com>
2023-08-29 12:11:30 -0300

    por hj é só pepepessoal

--- includes/topo.php
+++ includes/topo.php
@@ -1,19 +1,16 @@
 <?php
-session_start();
+    session_start();
 ?>
 
-<div class = "card">
-    <div class = "card-header">
-        <h1> PROJETO BLOG EM PHP + MYSQL IFSP - MAU MAU MORES</h1>
+<div class="card">
+    <div class="card-header">
+        <h1> Projeto Blog em PHP + MySQL IFSP - @MAU_MAU</h1>
     </div>
-
-    <php if (isset($_SESSION['login'])) :
-    ?>
-
-    <div class = "card-body text-right">
-        Olá <?php echo $_SESSION['login'] ['usuario'] ['nome'] ?>! 
-        <a href= "core/usuario_repositorio.php?acao=logaut"
-        class= "btn btn-link btn-sm" role = "button">Sair</a>
+    <?php if (isset($_SESSION['login'])): ?>
+    <div class="card-body text-right">
+        Olá <?php echo $_SESSION['login']['usuario']['nome']?>!
+        <a href="core/usuario_repositorio.php?acao=logout"
+        class="btn btn-link btn-sm" role="button">Sair</a>
     </div>
-    <?php endif ?> 
+    <?php endif ?>
 </div>
